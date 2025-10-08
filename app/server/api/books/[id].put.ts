cb4a3ec0601c7dbe4f9e6df1ee56153a5848dbf5
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2025-10-08 21:22:12 +0500

    feat:  add book update API endpoint

---
+++ app/server/api/books/[id].put.ts
@@ -0,0 +1,36 @@
+import BookModel from "~/server/models/Book.model";
+import { BookSchema } from "~/server/validation";
+import { defineEventHandler, readBody, createError } from "h3";
+import type { H3Event } from "h3";
+
+export default defineEventHandler( async(event:H3Event) =>{
+  //Get Body Data 
+  const body= await readBody(event);
+  //get id from params
+  const { id } = event.context.params as { id: string };
+  let { error } = BookSchema.validate(body,{abortEarly: true, allowUnknown: true});
+  if(error){
+    throw createError({
+      message: error.message.replace(/"/g, ""),
+      statusCode: 400,
+      fatal: false
+    })
+  }
+  //Update Book
+  try {
+    await BookModel.findByIdAndUpdate(id, body);
+    return {
+      message: "Author Updated"
+    }
+  } catch (e) {
+      if (e instanceof Error) {
+      throw createError({
+        statusMessage: e.message,
+      });
+    }
+    throw createError({
+      statusMessage: String(e),
+    });
+  }
+
+})
