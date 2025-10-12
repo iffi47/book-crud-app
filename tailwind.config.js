45a9a9325d056406f3e9248dd309fa173361a35c
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2025-10-12 15:28:55 +0500

    feat: add plugins and fix configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
- const colors = require('tailwindcss/colors');
- @type {import("tailwindcss").Config}
- module.exports= {
-  content:[],
-  theme: {
-    extend:{
-      fontFamily: {
-        sans: {"Inter"}
-      },
-      colors:{
-        primary: {
-          DEFAULT: colors.orange[500],
-          ...colors.orange
-        }
-      }
-    }
+// tailwind.config.js  (ESM because "type": "module")
+
+import colors from "tailwindcss/colors";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: [
+  "./components/**/*.{vue,js,ts}",
+  "./layouts/**/*.vue",
+  "./pages/**/*.vue",
+  "./app.vue",
+ ],
+ theme: {
+  extend: {
+   fontFamily: {
+    sans: ["Inter"],
+   },
+   colors: {
+    primary: colors.orange,
+   },
   },
-  plugins: [require("tailwindcss/forms"), require("@headlessui/tailwindcss")]
- }
+ },
+ plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
+};
