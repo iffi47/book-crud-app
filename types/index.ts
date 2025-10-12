f2008df19be93967ed3131d82b94b72a08540e33
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2025-10-12 18:56:50 +0500

    feat: add types

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+import { string, number } from 'joi';
+export interface IBook {
+  _id?:string;
+  title: string;
+  isbn: string;
+  pageCount: number;
+  published: string;
+  author: IAuthor
+}
+export interface IAuthor {
+  name: string;
+  _id?: string;
+}
